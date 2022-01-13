b067b01ae9200fdd00512845aa33a975b9490c87
joshua-hashimoto <dropscar041205@gmail.com>
2022-01-14 00:53:06 +0900

    lesson6 - モデル

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
+# https://docs.djangoproject.com/ja/4.0/ref/models/fields/#model-field-types
+
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=250)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    # TODO: サムネイルを追加する
+    # TODO: 作成者を追加する
+    # TODO: タグを追加する
