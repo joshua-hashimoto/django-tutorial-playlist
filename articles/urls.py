1d592cc699b1e44d55ac0656922c8084fde282ff
joshua-hashimoto <dropscar041205@gmail.com>
2022-01-16 22:48:19 +0900

    lesson13 - URL parameters

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import article_list
+from .views import article_list, article_detail
 
 app_name = "articles"
 
 
 urlpatterns = [
+    path("<slug:article_slug>/", article_detail, name="article_detail"),
     path("", article_list, name="article_list"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Article
 
@@ -9,4 +10,18 @@ def article_list(request):
         "object_list": articles,
     }
     template_name = "articles/article_list.html"
+    return render(request, template_name, context)
+
+
+def article_detail(request, article_slug):
+    # try:
+    #     article = Article.objects.get(slug=article_slug)
+    # except:
+    #     raise Http404
+
+    article = get_object_or_404(Article, slug=article_slug)
+    context = {
+        "object": article,
+    }
+    template_name = "articles/article_detail.html"
     return render(request, template_name, context)
